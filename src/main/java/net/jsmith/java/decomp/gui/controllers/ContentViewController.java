dddfa57790bf949138ae145f45fc09f85cc64008
Jacob Smith <jacob101607@yahoo.com>
2016-03-06 21:18:34 -0600

    Pull content view login into separate view.

--- src/main/java/net/jsmith/java/decomp/gui/controllers/ContentViewController.java
+++ src/main/java/net/jsmith/java/decomp/gui/controllers/ContentViewController.java
@@ -10,16 +10,13 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.TreeCell;
 import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import net.jsmith.java.decomp.gui.ContainerView;
 import net.jsmith.java.decomp.gui.ListenerUtils;
-import net.jsmith.java.decomp.gui.controls.PackageTreeItem;
-import net.jsmith.java.decomp.gui.controls.SortedTreeItem;
 import net.jsmith.java.decomp.gui.controls.TypeTreeItem;
-import net.jsmith.java.decomp.utils.TypeNameUtils;
+import net.jsmith.java.decomp.gui.controls.TypeTreeView;
 import net.jsmith.java.decomp.workspace.Container;
 import net.jsmith.java.decomp.workspace.Metadata;
 import net.jsmith.java.decomp.workspace.Type;
@@ -41,7 +38,7 @@ public class ContentViewController implements Controller {
 	private final ContainerView containerView;
 	
 	@FXML
-	private TreeView< String > contentTree;
+	private TypeTreeView contentTree;
 	
 	private ContentViewController( ContainerView containerView ) {
 		this.containerView = Objects.requireNonNull( containerView, "containerView" );
@@ -54,76 +51,19 @@ public class ContentViewController implements Controller {
     	}
     	containerView.openAndShowType( type );
 	}
-	
-	private SortedTreeItem getPackageTreeItem( Metadata metadata ) {
-		SortedTreeItem node = ( SortedTreeItem ) this.contentTree.getRoot( );
-        for( String pkgPart : TypeNameUtils.getPackageParts( metadata.getFullName( ) ) ) {
-        	SortedTreeItem nextNode = null;
-            for( TreeItem< String > child : node.getChildren( ) ) {
-                if( child.getValue( ).equals( pkgPart ) ) {
-                    nextNode = ( SortedTreeItem ) child;
-                    break;
-                }
-            }
-            if( nextNode == null ) {
-                nextNode = new PackageTreeItem( pkgPart );
-                node.addSortedChild( nextNode );
-            }
-            node = nextNode;
-        }
-        
-        return node;
-    }
-    
-    private SortedTreeItem findEnclosingTreeItem( Metadata metadata ) {
-    	String enclosingTypeName = TypeNameUtils.getEnclosingTypeName( metadata.getFullName( ) );
-    	if( enclosingTypeName == null ) {
-    		return null;
-    	}
-    	SortedTreeItem node = this.getPackageTreeItem( metadata );
-    	for( String namePart : TypeNameUtils.getTypeParts( enclosingTypeName ) ) {
-    		SortedTreeItem nextNode = null;
-    		for( TreeItem< String > child : node.getChildren( ) ) {
-    			if( child.getValue( ).equals( namePart ) ) {
-    				nextNode = ( SortedTreeItem ) child;
-    				break;
-    			}
-    		}
-    		if( nextNode == null ) {
-    			return null;
-    		}
-    		node = nextNode;
-    	}
-    	
-    	return node;
-    }
     
     private void addType( Type type ) {
     	Metadata metadata = type.getMetadata( );
     	if( LOG.isTraceEnabled( ) ) {
     		LOG.trace( "Recieved type loaded event for type '{}' from container '{}'.", metadata.getFullName( ), type.getContainer( ).getName( ) );
     	}
-    	if( type.getMetadata( ).getEnclosingType( ) != null ) {
+    	if( metadata.getEnclosingType( ) != null ) {
     		if( LOG.isTraceEnabled( ) ) {
     			LOG.trace( "Ignoring anonymous inner class '{}' in container '{}'.", metadata.getFullName( ), type.getContainer( ).getName( ) );
     		}
     		return;
     	}
-    	String typeName = metadata.getTypeName( );
-
-		// Attempt to resolve a parent type that may already
-		//  be loaded. Some java programmers decide to use '$'
-		//  into class names so we may not find them.
-    	SortedTreeItem enclosingItem = this.findEnclosingTreeItem( metadata );
-    	if( enclosingItem != null && enclosingItem instanceof TypeTreeItem ) {
-    		TypeTreeItem tti = ( TypeTreeItem ) enclosingItem;
-    		
-    		typeName = typeName.substring( tti.getType( ).getMetadata( ).getTypeName( ).length( ) + 1 );
-    		enclosingItem.addSortedChild( new TypeTreeItem( typeName, type ) );
-    	}
-    	else {
-    		getPackageTreeItem( metadata ).addSortedChild( new TypeTreeItem( typeName, type ) );
-    	}
+    	this.contentTree.addType( type );
     }
 	
 	@FXML
---
+++ src/main/java/net/jsmith/java/decomp/gui/controls/TypeTreeView.java
@@ -0,0 +1,78 @@
+package net.jsmith.java.decomp.gui.controls;
+
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import net.jsmith.java.decomp.utils.TypeNameUtils;
+import net.jsmith.java.decomp.workspace.Metadata;
+import net.jsmith.java.decomp.workspace.Type;
+
+public class TypeTreeView extends TreeView< String > {
+	
+	public TypeTreeView( ) {
+		this.setRoot( new PackageTreeItem( "" ) );
+		this.setShowRoot( false );
+	}
+	
+	public void addType( Type type ) {
+		Metadata metadata = type.getMetadata( );
+    	String typeName = metadata.getTypeName( );
+
+		// Attempt to resolve a parent type that may already
+		//  be loaded. Some java programmers decide to use '$'
+		//  into class names so we may not find them.
+    	SortedTreeItem enclosingItem = this.findEnclosingTreeItem( metadata );
+    	if( enclosingItem != null && enclosingItem instanceof TypeTreeItem ) {
+    		TypeTreeItem tti = ( TypeTreeItem ) enclosingItem;
+    		
+    		typeName = typeName.substring( tti.getType( ).getMetadata( ).getTypeName( ).length( ) + 1 );
+    		enclosingItem.addSortedChild( new TypeTreeItem( typeName, type ) );
+    	}
+    	else {
+    		getPackageTreeItem( metadata ).addSortedChild( new TypeTreeItem( typeName, type ) );
+    	}
+	}
+	
+	private SortedTreeItem getPackageTreeItem( Metadata metadata ) {
+		SortedTreeItem node = ( SortedTreeItem ) this.getRoot( );
+        for( String pkgPart : TypeNameUtils.getPackageParts( metadata.getFullName( ) ) ) {
+        	SortedTreeItem nextNode = null;
+            for( TreeItem< String > child : node.getChildren( ) ) {
+                if( child.getValue( ).equals( pkgPart ) ) {
+                    nextNode = ( SortedTreeItem ) child;
+                    break;
+                }
+            }
+            if( nextNode == null ) {
+                nextNode = new PackageTreeItem( pkgPart );
+                node.addSortedChild( nextNode );
+            }
+            node = nextNode;
+        }
+        
+        return node;
+    }
+    
+    private SortedTreeItem findEnclosingTreeItem( Metadata metadata ) {
+    	String enclosingTypeName = TypeNameUtils.getEnclosingTypeName( metadata.getFullName( ) );
+    	if( enclosingTypeName == null ) {
+    		return null;
+    	}
+    	SortedTreeItem node = this.getPackageTreeItem( metadata );
+    	for( String namePart : TypeNameUtils.getTypeParts( enclosingTypeName ) ) {
+    		SortedTreeItem nextNode = null;
+    		for( TreeItem< String > child : node.getChildren( ) ) {
+    			if( child.getValue( ).equals( namePart ) ) {
+    				nextNode = ( SortedTreeItem ) child;
+    				break;
+    			}
+    		}
+    		if( nextNode == null ) {
+    			return null;
+    		}
+    		node = nextNode;
+    	}
+    	
+    	return node;
+    }
+	
+}
